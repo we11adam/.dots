598788de337900330a13cc9dfa521a89e7c45e7e
Adam Lau <skyros@gmail.com>
2021-10-06 21:54:50 +0800

    chore: update

--- setup.sh
+++ setup.sh
@@ -1,10 +1,12 @@
-#curl -k -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
+#!/usr/bin/env bash
+##curl -k -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
 cd dots;
-for file in .*; 
+timestamp=$(date "+%Y-%m-%d-%H-%M-%S")
+for file in $(ls);
 do
-  if [ -f $HOME/$file ]; then
-    echo "$file exists, moving to $file.pre";
-    mv $HOME/$file $HOME/$file.pre
+  if [ -f $HOME/.$file ]; then
+    echo "$file exists, renamed to $file.orig.$timestamp";
+    mv $HOME/.$file $HOME/.$file.orig.$timestamp
    fi
-ln -s $HOME/.dots/dots/$file $HOME/$file; 
+ln -s $HOME/.dots/dots/$file $HOME/.$file;
 done
